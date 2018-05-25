b09520ae1be24bd1c4cf21c513ff8a08e59d88cf
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2018-05-25 11:39:03 +0200

    Import: Correct database and directory

--- import.sh
+++ import.sh
@@ -15,6 +15,6 @@ IMPORTER="/srv/deploy/monetdb-import"
 DIRECTORY="$HOME/upload/$ORGANIZATION/$DATE"
 DB="gros_$ORGANIZATION"
 
-python "$IMPORTER/Scripts/recreate_database.py" --force --no-table-import --no-schema --keep-jenkins -h "$HOST"
+python "$IMPORTER/Scripts/recreate_database.py" --force --no-table-import --no-schema --keep-jenkins -h "$HOST" -d "$DB"
 
-"$IMPORTER/Scripts/import_tables.sh" "$DIRECTORY" "$PATH" "$DB"
+"$IMPORTER/Scripts/import_tables.sh" "$DIRECTORY" "$DIRECTORY" "$DB"
